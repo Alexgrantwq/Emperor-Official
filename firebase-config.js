3f73081dcd97404101174ed5eb259eb07af659e4
Alexgrantwq <serenewaves899@gmail.com>
2025-05-07 04:02:41 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
-// Import the functions you need from the SDKs you need import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js"; import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+// Firebase configuration import { initializeApp } from "firebase/app"; import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration const firebaseConfig = { apiKey: "AIzaSyAlX0agj3nrgKLzLyJmtmxyW...", // Replace with your full API key authDomain: "emperor-official-1.firebaseapp.com", projectId: "emperor-official-1", storageBucket: "emperor-official-1.appspot.com", messagingSenderId: "965972364647", appId: "1:965972364647:web:cdcdadda0ff2eb60c473c2", measurementId: "G-63M9HXELL5" };
+const firebaseConfig = { apiKey: "AIzaSyAlX0agj3nrgKLZLyJmtmxyW...", authDomain: "emperor-official-1.firebaseapp.com", projectId: "emperor-official-1", storageBucket: "emperor-official-1.appspot.com", messagingSenderId: "965972364647", appId: "1:965972364647:web:cdcdadda0ff2eb60c473c2", measurementId: "G-03M9HXELL5" };
 
-// Initialize Firebase const app = initializeApp(firebaseConfig); const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig); const analytics = getAnalytics(app);
 
